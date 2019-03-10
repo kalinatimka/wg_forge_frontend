572caff6573ede03c390257adcf66f6d4aa67e9a
Timofey <kalinata0799@gmail.com>
2019-03-10 18:08:57 +0300

    Выполнил 2-й таск

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 // import 'orders' from '../data/orders.json';
 
 import orders from '../data/orders.json';
+import users from '../data/users.json';
+
 export default (function () {
     var template = document.querySelector('#row_template').content;
     var tbody = document.querySelector('tbody');
@@ -11,7 +13,7 @@ export default (function () {
         var row = template.cloneNode(true);
         row.querySelector('tr').id = 'order_' + order.id;
         row.querySelector('td:nth-child(1)').textContent = order.transaction_id;
-        row.querySelector('td:nth-child(2)').textContent = order.user_id;
+        row.querySelector('td:nth-child(2) a').textContent = getFullName(order.user_id);
         row.querySelector('td:nth-child(3)').textContent = getDate(order.created_at);
         row.querySelector('td:nth-child(4)').textContent = '$' + order.total;
         row.querySelector('td:nth-child(5)').textContent = hideCardNumber(order.card_number);
@@ -47,6 +49,30 @@ export default (function () {
         }
         cardNumber += mas[mas.length - 4] + mas[mas.length - 3] + mas[mas.length - 2] + mas[mas.length - 1];
         return cardNumber;
+    }
+
+    var getFullName = function (user_id) {
+        if (user_id <= users.length / 2) {
+            var i = 0;
+            while (1) {
+                if (user_id === users[i].id) { 
+                    var str = users[i].gender === 'Male' ? 'Mr. ' : 'Ms. ';
+                    str += users[i].first_name + ' ' + users[i].last_name;
+                    return str;
+                }
+                i++;
+            }
+        } else {
+            var i = users.length - 1;
+            while (1) {
+                if (user_id === users[i].id) { 
+                    var str = users[i].gender === 'Male' ? 'Mr. ' : 'Ms. ';
+                    str += users[i].first_name + ' ' + users[i].last_name;
+                    return str;
+                }
+                i--;
+            }
+        }
     }
 
     renderRows(orders);
