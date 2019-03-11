dfb6146f399b5d3cf46bd7bcfda468a8954b432c
Timofey <kalinata0799@gmail.com>
2019-03-11 12:22:47 +0300

    Выполнил 3-й таск

--- src/app.js
+++ src/app.js
@@ -3,22 +3,33 @@
 
 import orders from '../data/orders.json';
 import users from '../data/users.json';
+import companies from '../data/companies.json';
 
 export default (function () {
     var template = document.querySelector('#row_template').content;
     var tbody = document.querySelector('tbody');
-    console.log(tbody);
 
     var renderRow = function (order) {
+        var userInfo = getJSONData(order.user_id, users, 'id');
+        var companyInfo = getJSONData(userInfo.company_id, companies, 'id');
         var row = template.cloneNode(true);
+        var userDetails = row.querySelector('.user-details');
+        var toggleLink = row.querySelector('.user_toggle-link');
+
+        toggleLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            userDetails.classList.toggle('user-details--hide');
+        });
+
         row.querySelector('tr').id = 'order_' + order.id;
         row.querySelector('td:nth-child(1)').textContent = order.transaction_id;
-        row.querySelector('td:nth-child(2) a').textContent = getFullName(order.user_id);
+        row.querySelector('td:nth-child(2) a').textContent = getFullName(userInfo);
         row.querySelector('td:nth-child(3)').textContent = getDate(order.created_at);
         row.querySelector('td:nth-child(4)').textContent = '$' + order.total;
         row.querySelector('td:nth-child(5)').textContent = hideCardNumber(order.card_number);
         row.querySelector('td:nth-child(6)').textContent = order.card_type;
         row.querySelector('td:nth-child(7)').textContent = order.order_country + ' ('+ order.order_ip + ')';
+        renderDetails(row, userInfo, companyInfo);
         return row;
     }
 
@@ -32,7 +43,7 @@ export default (function () {
 
     var getDate = function (timestamp) {
         var date = new Date();
-        date.setTime(timestamp*1000);
+        date.setTime(timestamp * 1000);
         return date.getDate() +'/'+ (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + date.toLocaleTimeString('en-US', date.getHours);
     }
 
@@ -51,29 +62,69 @@ export default (function () {
         return cardNumber;
     }
 
-    var getFullName = function (user_id) {
-        if (user_id <= users.length / 2) {
+    var getJSONData = function (user_id, dataObject, field) {
+        if (user_id === null) {
+            return null;
+        }
+        if (user_id <= dataObject.length / 2) {
             var i = 0;
             while (1) {
-                if (user_id === users[i].id) { 
-                    var str = users[i].gender === 'Male' ? 'Mr. ' : 'Ms. ';
-                    str += users[i].first_name + ' ' + users[i].last_name;
-                    return str;
+                if (user_id === dataObject[i][field]) { 
+                    // console.log(dataObject[i][field] + ', ' + user_id);
+                    return dataObject[i];
                 }
                 i++;
             }
         } else {
-            var i = users.length - 1;
+            var i = dataObject.length - 1;
             while (1) {
-                if (user_id === users[i].id) { 
-                    var str = users[i].gender === 'Male' ? 'Mr. ' : 'Ms. ';
-                    str += users[i].first_name + ' ' + users[i].last_name;
-                    return str;
+                if (user_id === dataObject[i][field]) { 
+                    // console.log(dataObject[i][field] + ', ' + user_id);
+                    return dataObject[i];
                 }
                 i--;
             }
         }
+    }    
+
+    var getFullName = function (userInfo) {
+        var str = userInfo.gender === 'Male' ? 'Mr. ' : 'Ms. ';
+        str += userInfo.first_name + ' ' + userInfo.last_name;
+        return str;
+    }
+
+    var renderDetails = function (row, userInfo, companyInfo) {
+        if (userInfo.birthday != null) {
+            var date = new Date();
+            date.setTime(userInfo.birthday * 1000);
+            row.querySelector('.user_birthday').textContent = 'Birthday: ' + date.getDate() +'/'+ (date.getMonth() + 1) + '/' + date.getFullYear();
+        } else {
+            row.querySelector('.user_birthday').remove(); 
+        }
+
+        if (userInfo.avatar != null) {
+            row.querySelector('.user_image img').src = userInfo.avatar;
+        } else {
+            row.querySelector('.user_image').remove();
+        }
+
+        if (companyInfo != null) {
+            row.querySelector('.user_company a').href = companyInfo.url;
+            row.querySelector('.user_company a').textContent = companyInfo.title;
+            row.querySelector('.user_industry').textContent = 'Industry: ' + companyInfo.industry + ' / ' + companyInfo.sector;
+        } else {
+            row.querySelector('.user_company').remove();
+            row.querySelector('.user_industry').remove();
+            
+        }
     }
 
     renderRows(orders);
+
+    // link.addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     console.log('fff');
+    //     userDetails.classList.toggle('user-details--hide');
+    // })
+
 }());
